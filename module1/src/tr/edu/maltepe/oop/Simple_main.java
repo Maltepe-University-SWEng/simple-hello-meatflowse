7bebd8afa43f46ac138535a741b60b3498edf680
Meatflowse <kocemre2004@gmail.com>
2024-10-08 10:28:12 +0300

    Emre Koç 220706047

--- module1/src/tr/edu/maltepe/oop/Simple_main.java
+++ module1/src/tr/edu/maltepe/oop/Simple_main.java
@@ -1,10 +1,9 @@
 package tr.edu.maltepe.oop;
-public  class imple_main {
+public class Simple_main {
 
  public static void main(String []args){
         //
        System.out.println("simple main.....");
         }
 }
-
 
